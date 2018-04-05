206d6ecb43597239e9dc867f633464ff55e257a7
agarcia96 <angelicagarcia16@augustana.edu>
2018-04-04 20:59:51 -0500

    Added a color, modified text, changed a variable name

--- app/src/main/java/com/example/angelicagarcia16/speechrecognitiondemo/MainActivity.java
+++ app/src/main/java/com/example/angelicagarcia16/speechrecognitiondemo/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
+        mVoiceInputTv = (TextView) findViewById(R.id.voiceInputTV);
         mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
         mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something awesome!");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
